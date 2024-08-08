75b0853cb49a239991d08a9548e7bfad087a8a4e
Patrick Goodwin <pattygsocials@gmail.com>
2024-08-08 09:27:50 -0400

    Create print_directory_tree.py

---
+++ Print Directory Tree/print_directory_tree.py
@@ -0,0 +1,13 @@
+import os
+
+def print_directory_tree(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+directory_path = '/path/to/your/directory'
+print_directory_tree(directory_path)
