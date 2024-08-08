02d635b981ba1f3ec771f8e53281618ee2e5488e
Patrick Goodwin <pattygsocials@gmail.com>
2024-08-08 09:29:19 -0400

    Create generate_qr_code.py

---
+++ Generate QR Code/generate_qr_code.py
@@ -0,0 +1,17 @@
+import qrcode
+
+def generate_qr_code(data, file_path):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    img.save(file_path)
+
+data = 'https://www.example.com'
+file_path = 'qrcode.png'
+generate_qr_code(data, file_path)
